e75b8231c3bbfa3b0901da4d9e3892076eb835b4
d w <danny.wainshtein@gmail.com>
2014-04-22 21:56:39 +0300

    Starting?

---
+++ Numbers/PI_N_Digit/Python/find.py
@@ -0,0 +1,15 @@
+import sys
+
+def calc_n_digit_pi(n):
+    return n
+
+
+def main(argv=sys.argv):
+    n = input("Find what Pi Digit? ")        
+    print calc_n_digit_pi(n)
+
+
+if __name__ == "__main__":
+    main()
+
+
